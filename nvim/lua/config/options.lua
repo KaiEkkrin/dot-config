2b8a2daf52cb96fb2ca693c827822535b8191627
Alex Holloway <alex@alexholloway.me.uk>
2026-05-02 16:56:43 +0100

    Small update; enable OSC 52 clipboard

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,3 +7,17 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_node_provider = 0
+
+-- Use OSC 52 for clipboard (no xclip/xsel/wl-copy in the container;
+-- kitty + zellij both support OSC 52 passthrough)
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
